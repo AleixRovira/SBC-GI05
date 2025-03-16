5fcd356d9b24ed660aab02231aec5fae642c3ce0
adria <adria.mg@ext.salle.url.edu>
2025-03-16 19:11:38 +0100

    Acabat datasetGenerator.py. També canviat main i product per al nou dataset. Afegit gitignore per no incloure datasets.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from product import Product
 
+FILE_NAME = "datasets/products1.json"
 
 def read_dataset(filename: str) -> list:
     products = list()
@@ -8,10 +9,11 @@ def read_dataset(filename: str) -> list:
         data = json.load(json_file)
         for data_product in data:
             product = Product(data_product['nombre'], data_product['precio'], data_product['descripcion'],
-                              data_product['categoria'], data_product['marca'], data_product['colores'])
+                              data_product['categoria'], data_product['marca'], data_product['colores'],
+                              data_product['dispo_tallas'])
             products.append(product)
     return products
 
 if __name__ == '__main__':
-    filename = "datasets/products.json"
+    filename = FILE_NAME
     products = read_dataset(filename)
--- product.py
+++ product.py
@@ -1,11 +1,12 @@
 
 class Product:
-    def __init__(self, name: str, price: float, description: str, category: str, brand: str, colors: list) -> None:
+    def __init__(self, name: str, price: float, description: str, category: str, brand: str, colors: list, size_availability: list) -> None:
         self.name = name
         self.price = price
         self.description = description
         self.category = category
         self.brand = brand
         self.colors = colors
+        self.size_availability = size_availability
 
     
