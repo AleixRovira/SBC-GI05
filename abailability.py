8adb24c8f2d68e6b0d97f5346485e11a63c7ed68
Aleix Rovira <aleix.rovira@students.salle.url.edu>
2025-03-18 12:19:01 +0100

    crear_abailability

---
+++ abailability.py
@@ -0,0 +1,10 @@
+
+class abailability:
+    def __init__(self):
+        self.abailability = True
+
+    def get_abailability(product, size, color):
+
+        quantity = 0
+
+        return quantity
